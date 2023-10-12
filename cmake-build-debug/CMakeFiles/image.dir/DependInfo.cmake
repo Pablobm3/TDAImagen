42a1727e2e0d4d5fdffd774cd3f0d593cf058b7a
pablo <pablo.bazanmora@stud.h-da.de>
2023-10-12 12:38:03 +0200

    proyecto original

---
+++ cmake-build-debug/CMakeFiles/image.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pablo/Descargas/codigo/estudiante/src/image.cpp" "/home/pablo/Descargas/codigo/cmake-build-debug/CMakeFiles/image.dir/estudiante/src/image.cpp.o"
+  "/home/pablo/Descargas/codigo/estudiante/src/imageIO.cpp" "/home/pablo/Descargas/codigo/cmake-build-debug/CMakeFiles/image.dir/estudiante/src/imageIO.cpp.o"
+  "/home/pablo/Descargas/codigo/estudiante/src/imageop.cpp" "/home/pablo/Descargas/codigo/cmake-build-debug/CMakeFiles/image.dir/estudiante/src/imageop.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../estudiante/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
